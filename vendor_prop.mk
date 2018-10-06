87d7ee21d66a221d91e19307d8e5dbd47a13881d
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-10-06 20:35:34 +0530

    sanders: Enable Cloudflare DNS servers
    * seems to be faster + more privacy
    
    more info: https://1.1.1.1/

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -99,6 +99,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.core_ctl_min_cpu=2 \
     ro.vendor.qti.core_ctl_max_cpu=4
 
+# CloudFare DNS
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.dns1=1.1.1.1 \
+    net.dns2=1.0.0.1 \
+    net.rmnet0.dns1=1.1.1.1 \
+    net.rmnet0.dns2=1.0.0.1 \
+    net.gprs.dns1=1.1.1.1 \
+    net.gprs.dns2=1.0.0.1 \
+    net.ppp0.dns1=1.1.1.1 \
+    net.ppp0.dns2=1.0.0.1 \
+    net.wlan0.dns1=1.1.1.1 \
+    net.wlan0.dns2=1.0.0.1 \
+    net.eth0.dns1=1.1.1.1 \
+    net.eth0.dns2=1.0.0.1 \
+    dhcp.eth0.dns1=1.1.1.1 \
+    dhcp.eth0.dns2=1.0.0.1 \
+    dhcp.wlan0.dns1=1.1.1.1 \
+    dhcp.wlan0.dns2=1.0.0.1
+
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=192m \
