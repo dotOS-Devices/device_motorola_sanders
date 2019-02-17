bf0146780b8623b3fe263e5f16bffbfebff2fbb0
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-17 15:07:21 +0530

    sanders: dirac: Only call setEnabled on boot completed

--- MotoActions/src/com/moto/actions/BootCompletedReceiver.java
+++ MotoActions/src/com/moto/actions/BootCompletedReceiver.java
@@ -53,7 +53,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
 
         context.startService(new Intent(context, ServiceWrapper.class));
-        new DiracUtils(context);
+        new DiracUtils(context).onBootCompleted();
    }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
