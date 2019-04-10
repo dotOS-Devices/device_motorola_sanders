6754f87e0d2c0d9e4796576de6eebec2c832cb82
Artem Borisov <dedsa2002@gmail.com>
2019-04-10 14:20:31 +0000

    Sanders: Enforce RRO only for framework-res

--- device.mk
+++ device.mk
@@ -305,6 +305,9 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
