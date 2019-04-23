33287a7a772bd540d34410a7e329cc7485396276
Rajssss <sssraj.sssraj@gmail.com>
2019-04-23 11:05:56 +0000

    Added Gcam Mod. *GoogleCamera_6.2.024.239729896_N7P_test1.1 *AR working with all other features

--- device.mk
+++ device.mk
@@ -112,6 +112,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     libbson \
     libgui_vendor \
+    Gcam \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
