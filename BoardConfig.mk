de0fa6a17705f9f9594e803e25613fecfcc6fa76
jorg3lucas <jorgelucas.sd@gmail.com>
2019-04-27 05:23:32 +0000

    Disable Sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -222,8 +222,8 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
