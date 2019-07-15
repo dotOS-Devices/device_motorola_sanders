2230589b110c6ce5be41ccfe22ee368196ca543b
Lucas Ponez <lucasponez@outlook.com>
2019-07-15 18:46:14 +0000

    sanders: Add VENDOR SECURITY PATCH information
    Change-Id: I88b802573108d2f3b75224a67464e358c55a6dc3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -221,6 +221,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 # RIL
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2018-11-5
+
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
