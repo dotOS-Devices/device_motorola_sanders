a6484ecdbe4489c27561ece98cf4e2f60cf5666d
mendigodepijama <mendigodepijama@gmail.com>
2019-08-07 16:28:37 -0300

    sanders: gps: Remove all gps stuffs
    Change-Id: I55774f6d61e806bfed7290ce1b3e60098684b2f0

--- device.mk
+++ device.mk
@@ -176,20 +176,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-# GPS
-PRODUCT_PACKAGES += \
-    libgnss \
-    libgnsspps \
-    android.hardware.gnss@1.0-impl-qti
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
-
 # health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
