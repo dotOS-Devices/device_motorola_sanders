25f259bfdc12cb91c9822b03cbd43222cf42acfc
lucasponez <lucasponez@outlook.com>
2019-12-20 13:59:10 +0000

    sanders: sepolicy: Move to legacy-um
    Change-Id: Iab17b1975773435b141807aea9805aa7cd86737c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,7 +235,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2018-11-5
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Thermal
