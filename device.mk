6e681c7f8eb1e5d804292f077af299215efd1cd3
Łukasz Patron <priv.luk@gmail.com>
2020-10-24 17:11:31 +0200

    sanders: Build new aidl qti power HAL
    Change-Id: Ie8986e2b2b33b2a1f220795c4d270e92d6025fd1

--- device.mk
+++ device.mk
@@ -334,7 +334,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # Charger Images
 PRODUCT_PACKAGES += charger_res_images
