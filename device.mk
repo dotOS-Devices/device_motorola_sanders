a7ad779956910d00c0be8f64a64a24de83d8e42e
Dybios <dibyajyoti.samal@gmail.com>
2020-11-30 14:50:40 +0100

    sanders: Remove prebuilt GPS related commits
    Revert "sanders: Move GNSS vintf from vendor to local manifest"
    
    This reverts commit 9d2370d2fb28e8cdffe8db11006113a395086c37.
    
    Revert "sanders: Drop intree GPS hal since we moved fully to prebuilt"
    
    This reverts commit 28750e5bc5e11dd4443bdb76be4753db36185b2c.
    
    Change-Id: Ia2ed2c58c16cd0215ef053693489f251e857e5da

--- device.mk
+++ device.mk
@@ -197,13 +197,18 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service
 
 # GPS
+PRODUCT_PACKAGES += \
+    libgnss \
+    libgnsspps \
+    android.hardware.gnss@1.0-impl-qti
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
-    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # health
 PRODUCT_PACKAGES += \
@@ -458,6 +463,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf \
     WifiOverlay \
     TetheringOverlay \
+    libcurl \
     libQWiFiSoftApCfg \
     tcpdump
 
